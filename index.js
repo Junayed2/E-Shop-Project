0c56616972a8ca9a99fa6f5b5316665ab770231e
Junayed <muhammadjunayed2424@gmail.com>
2021-04-09 16:14:00 +0600

    added new

--- index.js
+++ index.js
@@ -32,9 +32,12 @@ client.connect((err) => {
     res.send("Hello Muhammad Junayed");
   });
   app.get("/products", (req, res) => {
-    productsCollection.find({}).toArray((err, documents) => {
-      res.send(documents);
-    });
+    const search = req.query.search;
+    productsCollection
+      .find({ name: { $regex: search } })
+      .toArray((err, documents) => {
+        res.send(documents);
+      });
   });
   app.get("/product/:key", (req, res) => {
     productsCollection
